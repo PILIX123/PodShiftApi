7a1f2d4ffb959380b262588ad60992b002d3bbed
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2024-03-03 06:56:11 -0500

    figuring stuff out

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 
-app = FastAPI()
+app = FastAPI(debug=__debug__)
 
 
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post('/PodShift')
+async def addFeed():
+    return {"message": "content"}
