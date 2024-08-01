ba9409c5f1a1bdcce2984dddcec00aea1c0189ec
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2024-08-01 02:50:20 -0400

    progress on the creation of the RSS

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, Depends
-from models.forminputmodel import FormInputModel
 from pyPodcastParser.Podcast import Podcast as pc
 from pyPodcastParser.Item import Item
 from requests import get
 from dateutil.rrule import rrule
-from contextlib import asynccontextmanager
+from dateutil.parser import parse
 from db import init_db, get_session
+from sqlmodel import Session, select
+from contextlib import asynccontextmanager
+from models.forminputmodel import FormInputModel
 from models.episode import Episode
 from models.podcast import Podcast
 from models.custompodcast import CustomPodcast
-from sqlmodel import Session, select
 from datetime import datetime
 import json
+from xml.etree import ElementTree as ET
 
 
 @asynccontextmanager
@@ -120,4 +122,69 @@ async def addTestPodcast(form: FormInputModel, session: Session = Depends(get_se
 @app.get("/PodShift/{customPodcastGUID}")
 async def getCustomFeed(customPodcastGUID, session: Session = Depends(get_session)):
     customFeed = session.get(CustomPodcast, customPodcastGUID)
-    return customFeed
+    podcast = customFeed.podcast
+
+    namespaces = {
+        "itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd"
+    }
+    rss = ET.Element("rss", version='2.0', nsmap=namespaces)
+    channel = ET.SubElement(rss, "channel")
+    ET.SubElement(channel, "title").text = podcast.title,
+    ET.SubElement(channel, "copyright").text = podcast.copyright
+    ET.SubElement(channel, "creative_commons").text = podcast.creative_commons
+    ET.SubElement(channel, "description").text = podcast.description
+    ET.SubElement(channel, "generator").text = podcast.generator
+    ET.SubElement(channel, "language").text = podcast.language
+    ET.SubElement(channel, "pubDate").text = podcast.published_date
+
+    # Itunes
+    ET.SubElement(channel,
+                  f"{namespaces.get("itunes")}author")\
+        .text = podcast.itunes_author_name
+    ET.SubElement(channel, f"{namespaces.get("itunes")}block")\
+        .text = podcast.itunes_block
+    ET.SubElement(channel, f"{namespaces.get("itunes")}complete")\
+        .text = podcast.itunes_complete
+    ET.SubElement(channel, f"{namespaces.get("itunes")}explicit")\
+        .text = podcast.itunes_explicit
+    ET.SubElement(channel, f"{namespaces.get("itunes")}image", href=podcast.itune_image)\
+        .text = podcast.itunes_complete
+    if podcast.itunes_new_feed_url:
+        ET.SubElement(channel, f"{namespaces.get("itunes")}new-feed-url")\
+            .text = podcast.itunes_new_feed_url
+
+    # Image
+    image = ET.SubElement(channel, "image")
+    ET.SubElement(image, "url").text = podcast.image_url
+    ET.SubElement(image, "link").text = podcast.image_link
+    ET.SubElement(image, "width").text = podcast.image_width
+    ET.SubElement(image, "height").text = podcast.image_height
+    ET.SubElement(image, "title").text = podcast.image_title
+
+    feed = rfeed.Feed(
+        title=f"Custom {podcast.title} ditribution",
+        link=podcast.link,
+        description=podcast.description,
+        language=podcast.language,
+        copyright=podcast.copyright,
+        managingEditor=podcast.managing_editor,
+        webMaster=podcast.web_master,
+        pubDate=podcast.published_date,
+        lastBuildDate=podcast.last_build_date,
+        categories=None,
+        generator=podcast.generator,
+        ttl=None,
+        image=rfeed.Image(
+            podcast.image_url,
+            podcast.image_title,
+            podcast.image_link,
+            podcast.image_width,
+            podcast.image_height,
+        ),
+    )
+    for date in json.loads(customFeed.dateToPostAt):
+        dt = parse(date)
+        if (dt > datetime.now()):
+            return
+        episodes = customFeed.podcast.episodes
+    return
