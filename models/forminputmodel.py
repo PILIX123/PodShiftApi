c4e6e31932d2a8affc26b79d302513dee1d74ac7
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2024-08-04 21:31:35 -0400

    found the code

---
+++ models/forminputmodel.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from enum import IntEnum
+from dateutil.rrule import WEEKLY, DAILY, MONTHLY, YEARLY
+
+
+class ReccurenceEnum(IntEnum):
+    WEEKLY = WEEKLY,  # 2
+    DAILY = DAILY,  # 3
+    MONTHLY = MONTHLY,  # 1
+    YEARLY = YEARLY  # 0
+
+
+class FormInputModel(BaseModel):
+    url: str
+    amountOfEpisode: int = 1
+    recurrence: ReccurenceEnum
+    everyX: int = 1
