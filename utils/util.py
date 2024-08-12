ba6359a1b2716431480b55908b0c16052852bd44
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2024-08-12 02:32:31 -0400

    added some tests for the util file

--- utils/util.py
+++ utils/util.py
@@ -1,5 +1,7 @@
 from dateutil.rrule import rrule
-from datetime import datetime, date
+from datetime import date
+
+# TODO: Determine what to do when uneven number of episodes
 
 
 def dateListRRule(freq: int, date: date, interval: int, nbEpisodes: int, amount: int) -> list[str]:
