e2ca50443c3f218ffb9e3bcd684be2e675e4a0c7
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2024-08-12 23:50:36 -0400

    refactored

--- main.py
+++ main.py
@@ -25,47 +25,6 @@ from utils.xml_reader import createPodcast, extractContents
 from utils.util import dateListRRule
 from cronjob import updateFeeds
 
-# def updateFeeds():
-#     session = next(get_session())
-#     stmnt = select(Podcast)
-#     r = session.exec(stmnt)
-
-#     for podcast in r:
-#         print(podcast.url)
-#         feed = ET.fromstring(get(podcast.url).content.decode())
-#         channel = feed.find("channel")
-#         episodesFromFeed = [ET.tostring(item, encoding="unicode")
-#                             for item in channel.findall("item")]
-#         latestEpisode = ET.tostring(
-#             channel.find("item"), encoding='unicode')
-#         latestDbEpisode = podcast.episodes[-1]
-#         if latestEpisode == latestDbEpisode.xml:
-#             continue
-#         else:
-#             podcast.episodes.append(Episode(
-#                 xml=latestEpisode, podcast=podcast))
-#             session.commit()
-#             for subcription in podcast.customPodcasts:
-#                 dateToPostAt = json.loads(subcription.dateToPostAt)
-#                 startFreq = dateToPostAt[0]
-#                 newRrule = rrule(
-#                     freq=subcription.freq,
-#                     dtstart=parse(startFreq),
-#                     interval=subcription.interval,
-#                     count=len(podcast.episodes)/subcription.amount
-#                 )
-#                 subcription.dateToPostAt = json.dumps(
-#                     [date.isoformat() for date in list(newRrule)])
-#                 session.commit()
-#         session.refresh(podcast)
-#         for episode, feedEpisode in zip(reversed(podcast.episodes), episodesFromFeed):
-#             if episode.xml == feedEpisode:
-#                 continue
-#             else:
-#                 episode.xml = feedEpisode
-#                 session.commit()
-#     session.close()
-
 ENVIRONEMENT_URL = "localhost:8000" if os.getenv(
     "DEBUG") else "podshift.ddns.net:8080"
 LOGGING_CONFIG["formatters"]["access"]["fmt"] = "%(asctime)s " + \
